dc5ea43f9a9234844595b728e23fab334c083461
noah <43697087+noahsterner@users.noreply.github.com>
2026-09-12 21:41:47 +0200

    feat(BLE): add charactersitc datatypes and moved read, update and update method to it

--- src/main.py
+++ src/main.py
@@ -1,43 +1,62 @@
-from abc import abstractmethod
-from typing import Any
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, Callable
 
 from bluezero import async_tools
 from bluezero.peripheral import Peripheral
 from bluezero import adapter
 
-class Service:
+@dataclass
+class charactersitic:
+    """Base class for a GATT characteristic.
+
+    A charactersitics belongs to a service and defines the behaviour for reading, writing and notifications.
+    """
+    srv_id: int
+    chr_id: int
+    uuid: str
+    flags: list[str]
+    value: list[Any]
+    notifying: bool
+
+    def read_value(self) -> list[int]:
+        """Reads the value of the characteristic
+
+        Return the characteristic value as a list of uint8 values.
+        """
+        raise NotImplementedError
+
+    def update_value(self, charactersitics: charactersitic) -> bool:
+        """Updates the characteristic value."""
+        raise NotImplementedError
+
+    def notify(self, notifying: bool, charactersitic: charactersitic) -> None:
+        """Toggle notifications."""
+        raise NotImplementedError
+
+class service(ABC)  :
     """Base class for a GATT service.
 
-    A service owns its own charactersitics and provides the callbacks required by bluezero
+    A service owns its own charactersitics and are responsible for registering them
     """
 
-    UUID: str
+    uuid: str
+    srv_id: int
 
     def __init__(self) -> None:
-        self._charactersitcs: list[Any] = []
+        self._charactersitcs: list[charactersitic] = []
 
     @abstractmethod
     def setup(self, peripheral: Peripheral) -> None:
-        """Register this service and its charactersitics wit bluezero"""
+        """Register this service and its charactersitics with bluezero"""
         raise NotImplementedError
 
-    def read_value(self) -> None:
-        """Called when a BLE clients reads a charactersitics"""
-        raise NotImplementedError
-
-    def update_value(self, charactersitics) -> None:
-        """Called when a BLE central updates a charactersitics value"""
-        raise NotImplementedError
-
-    def notify(self, notifying, charactersitics) -> None:
-        """Called when a BLE central subscribes/unsubscribse from charactersitics notification"""
-        pass
 
 class gatt_server:
     def __init__(self) -> None:
         self._adapter_address: str = list(adapter.Adapter.available())[0].address
         self._peripheral: Peripheral = Peripheral(self._adapter_address, "MyRobot")
-        self._services: list[Service] = []
+        self._services: list[service] = []
 
     def start(self):
         """start sending out advertising packages"""
