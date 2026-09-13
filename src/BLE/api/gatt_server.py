b6493b5d81bad0ec47afe4c5d3980c0c40a02392
noah <43697087+noahsterner@users.noreply.github.com>
2026-09-13 18:35:57 +0200

    feat(BLE): log when device connects and disconnects

--- src/BLE/api/gatt_server.py
+++ src/BLE/api/gatt_server.py
@@ -1,6 +1,8 @@
 import logging
 
-from service import Service
+from bluezero.device import Device
+
+from BLE.api.service import Service
 
 from bluezero.peripheral import Peripheral
 from bluezero import adapter
@@ -8,14 +10,17 @@ from bluezero import adapter
 logger = logging.getLogger(__name__)
 
 class GattServer:
-    def __init__(self, services: list[Service]) -> None:
+    def __init__(self, name: str, services: list[Service]) -> None:
         self._adapter_address: str = list(adapter.Adapter.available())[0].address
-        self._peripheral: Peripheral = Peripheral(self._adapter_address, "MyRobot")
+        self._peripheral: Peripheral = Peripheral(self._adapter_address, local_name=name)
         self._services: list[Service] = services
 
     def start(self) -> None:
         """start sending out advertising packages"""
         logger.info("Start GATT Server")
+
+        self._peripheral._on_connect = self._on_connect
+        self._peripheral._on_disconncet = self._on_disconncet
 
         self._setup_services()
         self._peripheral.publish()
@@ -26,3 +31,9 @@ class GattServer:
 
         for service in self._services:
             service.setup(self._peripheral)
+
+    def _on_connect(self, device: Device) -> None:
+        logger.info("Central connected: %s", device)
+
+    def _on_disconncet(self, adapter_address: str, device_address: str) -> None:
+        logger.info("Central disconnected: %s", adapter_address, device_address)
