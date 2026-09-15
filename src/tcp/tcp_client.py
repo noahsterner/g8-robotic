e441f554881acb5adf209100415bfe0ed31ade6c
noah <43697087+noahsterner@users.noreply.github.com>
2026-09-15 07:42:01 +0200

    feat(TCP): add write and recv method to TcpClient class

--- src/tcp/tcp_client.py
+++ src/tcp/tcp_client.py
@@ -13,10 +13,37 @@ class TcpClient:
 
     def connect(self):
         if not self._socket:
-            logger.info("Connected to tcp server: %s:%d", self._host, self._port)
             self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self._socket.connect((self._host, self._port))
+
+            logger.info("Connected to tcp server: %s:%d", self._host, self._port)
+
+    def _write(self, message):
+        if not self._socket:
+            raise RuntimeError("client not connected to a tcp socket")
+
+        data = bytearray((message + "\n").encode("utf-8"))
+        self._socket.sendall(data)
+
+    def _recv(self) -> bytearray:
+        if not self._socket:
+            raise RuntimeError("client not connected to a tcp socket")
+
+        message = bytearray()
+        while True:
+            byte = self._socket.recv(1)
+
+            if byte == b"\n":
+                break
+
+            message.extend(byte)
+
+        return message
 
 if __name__ == "__main__":
     client = TcpClient("localhost", 4711)
     client.connect()
+    client._write("Hello, from xyz-robotic")
+    recv = client._recv().decode("utf-8")
+    print(recv)
+
