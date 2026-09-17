df671ce7974706b7560488906c71f0ee7b9ab489
noah <43697087+noahsterner@users.noreply.github.com>
2026-09-18 01:10:32 +0200

    feat: add notification

--- src/ble/characteristics/battery_service/battery_level_characteristic.py
+++ src/ble/characteristics/battery_service/battery_level_characteristic.py
@@ -1,5 +1,5 @@
 import logging
-
+from bluezero import async_tools
 from ble.api.characteristic import Characteristic
 from ble.api.flags import CharacteristicFlags
 from tcp.tcp_client import TcpClient
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 class BatteryLevelCharacteristic(Characteristic):
     characteristic_id = 1
     uuid = "FF11"
-    flags = [CharacteristicFlags.READ]
+    flags = [CharacteristicFlags.READ, CharacteristicFlags.NOTIFY]
     value = [100]
     notifying = False
 
@@ -17,6 +17,19 @@ class BatteryLevelCharacteristic(Characteristic):
         super().__init__(tcp_client, service_id)
 
     def read_value(self) -> list[int]:
-        self.value[0] -= 1
+        self.value[0] = self._tcp_client.request("Battery.GetBatteryLevel()")
         logger.info("read battery level: %s", self.value)
+
         return self.value
+
+    def notify(self, notifying: bool, characteristic) -> None:
+        self.notifying = notifying
+        if(self.notifying):
+            async_tools.add_timer_seconds(10, self._pool_battery_level)
+
+    def _pool_battery_level(self):
+        self.value[0] = self._tcp_client.request("Battery.GetBatteryLevel()")
+        logger.info("battery level pooled: %s", self.value)
+
+
+
